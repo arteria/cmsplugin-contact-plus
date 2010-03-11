0a7440399ec1f396877d2132c2bfe3b7d3937d05
Robert <rtpm@gazeta.pl>
2010-03-11 11:00:14 -0700

    fixed imports

--- contact/cms_plugins.py
+++ contact/cms_plugins.py
@@ -1,8 +1,8 @@
 from django.utils.translation import ugettext_lazy as _
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from plugins.contact.models import Contact
-from plugins.contact.forms import ContactForm
+from models import Contact
+from forms import ContactForm
 
 class ContactPlugin(CMSPluginBase):
     model = Contact
