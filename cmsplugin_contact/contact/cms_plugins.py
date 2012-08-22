96045e52c3ed10411add2897e4d2b8aaeebee44b
Philippe O. Wagner <wagner@arteria.ch>
2012-08-22 17:14:43 +0200

    no message

--- cmsplugin_contact/contact/cms_plugins.py
+++ cmsplugin_contact/contact/cms_plugins.py
@@ -8,7 +8,7 @@ class ContactPlugin(CMSPluginBase):
     model = Contact
     name = _("Contact Form")
     render_template = "contact.html"
-    
+    """
     def render(self, context, instance, placeholder):
         request = context['request']
         if request.method == "POST":
@@ -33,9 +33,34 @@ class ContactPlugin(CMSPluginBase):
                 'contact': instance,
                 'form': form,
                     })
-                return context
+        return context
 
-
+    """
+    def render(self, context, instance, placeholder):
+    	request = context['request']
+    
+    	if request.method == "POST":
+    		form = ContactForm(request.POST)
+    		form.base_fields['email'].required = instance.email_required
+    		form.base_fields['phone'].required = instance.phone_required
+    		form.base_fields['subject'].required = instance.subject_required
+    		if form.is_valid():
+    			form.send(instance.site_email)
+    			context.update( {
+    				'contact': instance,
+    				})
+    			return context
+    	else:
+    		form = ContactForm()
+            form.base_fields['email'].required = instance.email_required
+            form.base_fields['phone'].required = instance.phone_required
+            form.base_fields['subject'].required = instance.subject_required
+    	
+            context.update({
+    		'contact': instance,
+    		'form': form,
+            	})
+            return context
         
     
 plugin_pool.register_plugin(ContactPlugin)
