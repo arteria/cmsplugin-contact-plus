0dc25853041ab74ae9ed1afee2ec69c33c99fcc9
Philippe O. Wagner <wagner@arteria.ch>
2012-08-22 16:58:15 +0200

    no message

--- cmsplugin_contact/contact/models.py
+++ cmsplugin_contact/contact/models.py
@@ -5,18 +5,20 @@ from cms.models.pluginmodel import CMSPlugin
 from django.conf import settings
 
 class Contact(CMSPlugin):
-	site_email = models.EmailField(_('Email reciepient'))
-	email_label = models.CharField(_('Email sender label'), max_length=100)
-	email_required = models.BooleanField(default=True, _('Uncheck if optional'))
-	phone_label = models.CharField(_('Phone label'), blank=True, help_text=_("Optional if empty"), max_length=100)
-	phone_required = models.BooleanField(default=True, _('Uncheck if optional'))
-	subject_label = models.CharField(_('Subject label'), help_text=_("Use as generic field"), max_length=200)
-	subject_required = models.BooleanField(default=True, _('Uncheck if optional'))
-	content_label = models.CharField(_('Message content label'), max_length=100)
-	thanks = models.CharField(_('Message displayed on successful submit'), max_length=200)
-	submit = models.CharField(_('Submit button value'), blank=True, max_length=30)
-    inclose = models.CharField(max_length=100, choices=getattr(settings, 'AR_GRID_CLASSES', (('', ''),), default="")
+    site_email = models.EmailField(_('Email reciepient'))
+    email_label = models.CharField(_('Email sender label'), max_length=100)
+    email_required = models.BooleanField(default=True, _('Uncheck if optional'))
+    phone_label = models.CharField(_('Phone label'), blank=True, help_text=_("Optional if empty"), max_length=100)
+    phone_required = models.BooleanField(default=True, _('Uncheck if optional'))
+    subject_label = models.CharField(_('Subject label'), help_text=_("Use as generic field"), max_length=200)
+    subject_required = models.BooleanField(default=True, _('Uncheck if optional'))
+    content_label = models.CharField(_('Message content label'), max_length=100)
+    thanks = models.CharField(_('Message displayed on successful submit'), max_length=200)
+    submit = models.CharField(_('Submit button value'), blank=True, max_length=30)
+    inclose = models.CharField(max_length=100, choices=getattr(settings, 'AR_GRID_CLASSES', (('', ''),)), default="")
+        
     
     
-	def __unicode__(self):
-		return self.site_email
+    
+    def __unicode__(self):
+        return self.site_email
