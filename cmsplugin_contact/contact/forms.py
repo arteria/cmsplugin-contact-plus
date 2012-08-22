a82a7edd1b0df5d18b53fb22c3766768b8d7b726
Philippe O. Wagner <wagner@arteria.ch>
2012-08-22 16:58:40 +0200

    no message

--- cmsplugin_contact/contact/forms.py
+++ cmsplugin_contact/contact/forms.py
@@ -3,21 +3,21 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 
 class ContactForm(forms.Form):
-	email 	= forms.EmailField(required=False) 
-	phone 	= forms.CharField(required=False)
-	subject	= forms.CharField()
-	content	= forms.CharField(widget=forms.Textarea())
+    email 	= forms.EmailField(required=False) 
+    phone 	= forms.CharField(required=False)
+    subject	= forms.CharField()
+    content	= forms.CharField(widget=forms.Textarea())
 
         def send(self, site_email):
                 email_message = EmailMessage(
-					self.cleaned_data['subject'],
-        				render_to_string("email.txt", {
-							'data': self.cleaned_data,
-					}),
-            				site_email,
-            				[site_email],
-            				headers = {
-						'Reply-To': self.cleaned_data['email']
-						},)
+                    self.cleaned_data['subject'],
+                        render_to_string("email.txt", {
+                            'data': self.cleaned_data,
+                    }),
+                            site_email,
+                            [site_email],
+                            headers = {
+                        'Reply-To': self.cleaned_data['email']
+                        },)
                 email_message.send(fail_silently=True)
-	
+    
