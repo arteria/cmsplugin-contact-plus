2ebcc2049a39462f099c0733a779779ea214f5a2
Philippe O. Wagner <wagner@arteria.ch>
2012-08-22 17:15:51 +0200

    no message

--- cmsplugin_contact/contact/cms_plugins.py
+++ cmsplugin_contact/contact/cms_plugins.py
@@ -37,29 +37,29 @@ class ContactPlugin(CMSPluginBase):
 
     """
     def render(self, context, instance, placeholder):
-    	request = context['request']
+        request = context['request']
     
-    	if request.method == "POST":
-    		form = ContactForm(request.POST)
-    		form.base_fields['email'].required = instance.email_required
-    		form.base_fields['phone'].required = instance.phone_required
-    		form.base_fields['subject'].required = instance.subject_required
-    		if form.is_valid():
-    			form.send(instance.site_email)
-    			context.update( {
-    				'contact': instance,
-    				})
-    			return context
-    	else:
-    		form = ContactForm()
+        if request.method == "POST":
+            form = ContactForm(request.POST)
             form.base_fields['email'].required = instance.email_required
             form.base_fields['phone'].required = instance.phone_required
             form.base_fields['subject'].required = instance.subject_required
-    	
+            if form.is_valid():
+                form.send(instance.site_email)
+                context.update( {
+                    'contact': instance,
+                    })
+                return context
+        else:
+            form = ContactForm()
+            form.base_fields['email'].required = instance.email_required
+            form.base_fields['phone'].required = instance.phone_required
+            form.base_fields['subject'].required = instance.subject_required
+        
             context.update({
-    		'contact': instance,
-    		'form': form,
-            	})
+            'contact': instance,
+            'form': form,
+                })
             return context
         
     
