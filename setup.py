539d6aa3967d9f569a8a56c1516cfeaa5476c3b2
Philippe O. Wagner <wagner@arteria.ch>
2012-08-22 09:57:50 +0200

    no message

---
+++ setup.py
@@ -0,0 +1,13 @@
+ 
+from distutils.core import setup 
+
+setup(
+     name='cmsplugin_contact_app',
+     version='0.1',
+     packages=['cmsplugin_contact',],
+     license='not defined',
+     long_description=open('README.txt').read(),
+     author='author',
+     author_email='email',
+)
+        
