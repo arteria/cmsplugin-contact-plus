80343b5f447ac2d14100df183e3a31397f82c738
Philippe O. Wagner <wagner@arteria.ch>
2012-08-22 17:01:29 +0200

    no message

--- cmsplugin_contact/contact/forms.py
+++ cmsplugin_contact/contact/forms.py
@@ -8,16 +8,16 @@ class ContactForm(forms.Form):
     subject	= forms.CharField()
     content	= forms.CharField(widget=forms.Textarea())
 
-        def send(self, site_email):
-                email_message = EmailMessage(
-                    self.cleaned_data['subject'],
-                        render_to_string("email.txt", {
-                            'data': self.cleaned_data,
-                    }),
-                            site_email,
-                            [site_email],
-                            headers = {
-                        'Reply-To': self.cleaned_data['email']
-                        },)
-                email_message.send(fail_silently=True)
-    
+    def send(self, site_email):
+        email_message = EmailMessage(
+            self.cleaned_data['subject'],
+                render_to_string("email.txt", {
+                    'data': self.cleaned_data,
+            }),
+                    site_email,
+                    [site_email],
+                    headers = {
+                'Reply-To': self.cleaned_data['email']
+                },)
+        email_message.send(fail_silently=True)
+
