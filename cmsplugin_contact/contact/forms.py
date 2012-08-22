9dbeac91886c4eaad08dd76a6e22d9f8a0e5709f
Philippe O. Wagner <wagner@arteria.ch>
2012-08-22 18:18:56 +0200

    no message

--- cmsplugin_contact/contact/forms.py
+++ cmsplugin_contact/contact/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
+from django.contrib.sites.models import Site
+from django.conf import settings
+
+
 
 class ContactForm(forms.Form):
     email 	= forms.EmailField() 
@@ -9,8 +13,11 @@ class ContactForm(forms.Form):
     content	= forms.CharField(widget=forms.Textarea())
 
     def send(self, site_email):
+        current_site = Site.objects.get_current()
+        
         email_message = EmailMessage(
-            self.cleaned_data['subject'],
+            # self.cleaned_data['subject'],
+            "NEW MESSAGE FROM " + current_site.domain.upper(),
                 render_to_string("email.txt", {
                     'data': self.cleaned_data,
             }),
