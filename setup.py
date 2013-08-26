852185ed2da88930ef658b7c8999bccb69339ae9
Philippe O. Wagner <wagner@arteria.ch>
2013-08-26 02:33:56 +0200

    Modified ReadMe file format

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
      license='BSD License',
      url='https://github.com/arteria/cmsplugin-contact-plus/',
      description='A django-CMS plugin to dynamically create Contact Forms.',
-     long_description=open('README.rst').read(),
+     long_description=open('README.md').read(),
      author='arteria GmbH',
      author_email='admin@arteria.ch',
 )
