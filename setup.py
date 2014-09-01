5b162003a6d595be9ae68c40480afa2601561579
Philippe O. Wagner <wagner@arteria.ch>
2014-09-01 23:02:25 +0200

    Version bump to 1.1.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'cmsplugin_contact_plus',
-    version = '1.1.9', 
+    version = '1.1.10', 
     packages=find_packages(),
     license = 'BSD License',
     url = 'https://github.com/arteria/cmsplugin-contact-plus/',
